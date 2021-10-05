dcae4daffe64e3e2afb614ab61d8cd22e6b40e15
thakaj <taulant.hakaj@scranton.edu>
2021-10-05 12:31:56 -0400

    react frontend

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module DansWebsite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

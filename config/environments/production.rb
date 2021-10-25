4e82fe1f42660a06a382c31cda050702896575b0
thakaj <taulant.hakaj@scranton.edu>
2021-10-25 14:11:32 -0400

    changed the config files to change the default waymy pdfs are updated. Previously deleting the file after each update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-
+  config.active_storage.replace_on_assign_to_many = false
   #devise gem
   #config.action_mailer.default_url_options = :host
 

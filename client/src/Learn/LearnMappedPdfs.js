11afcafebd7e1d2d228785a57b27028906b408fd
thakaj <taulant.hakaj@scranton.edu>
2021-10-25 17:44:33 -0400

    changed some of my componet

--- client/src/Learn/LearnMappedPdfs.js
+++ client/src/Learn/LearnMappedPdfs.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 
 function LearnMappedPdfs(){
     const [instruments, setInstruments] = useState([])
+    const isDisplayed = false
 
     useEffect( () => {
         fetch("/instruments")
@@ -10,17 +11,32 @@ function LearnMappedPdfs(){
             setInstruments(data)})
     },[])
 
+    function handleClick(){
+        
+    }
+
+    function handleDownload(id){
+        
+
+    }
+
+   
+
+
     return (
         <div>
             <div>
                 {instruments.map((i, index) => {
                     return (
                         <div key={index}>
-                       <h4>{i.name}</h4>
+                            <div style={{display: "flex"}}>
+                            <h4>{i.name}</h4>
+                            <button onclick={()=> {}}></button>
+                                    </div>
                         {i.pdfs.map((p, i)=> (
                             <article key={i}>
                                 <h6>{p.filename}</h6>
-                                <a href={p.url}> Download</a>
+                                <button >Download Pdf</button>
                             </article>
                         ))}
                    </div>

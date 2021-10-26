f6df0b3f73d0f0c00068d9991751686aa7c249a8
thakaj <taulant.hakaj@scranton.edu>
2021-10-26 15:47:39 -0400

    feels so good to finally fix this bug. I added proper functionality for downloading files

--- client/src/Learn/LearnMappedPdfs.js
+++ client/src/Learn/LearnMappedPdfs.js
@@ -1,47 +1,96 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
+import Select from 'react-select'
 
 function LearnMappedPdfs(){
     const [instruments, setInstruments] = useState([])
-    const isDisplayed = false
-
-    useEffect( () => {
-        fetch("/instruments")
-        .then(r => r.json())
-        .then(data=> {
-            setInstruments(data)})
-    },[])
-
-    function handleClick(){
-        
-    }
-
-    function handleDownload(id){
-        
-
-    }
-
+    const [pdf, setPdf] = useState([{
+        filename: "",
+        id: "",
+        url: ""
+    }])
+    const [show, setShow] = useState(null)
    
 
+    const [selectedValue, setSelectedValue] = useState("")
+
+    const options = [
+        {
+          "value": 1,
+          "label": "Bass"
+        },
+        {
+          "value": 2,
+          "label": "Upright Bass"
+        },
+        {
+          "value": 3,
+          "label": "Guitar"
+        },
+        {
+            "value": 4,
+            "label": "Piano"
+        },
+        {
+            "value": 5,
+            "label": "Drums"
+        }
+    ]
+    function handleInstrumentChange(obj){
+        setSelectedValue(obj.value)
+        fetch(`instruments/${obj.value}`)
+        .then(r => r.json())
+        .then(data=> {
+            setPdf(data.pdfs)
+            setInstruments(data)})
+        setShow(true)
+    }
+    
+    function handleDownload(url,doc){
+        fetch(url, {
+            method: "GET",
+            headers: {
+                Authorization: localStorage.getItem("token")
+        }})
+        .then((response) => response.blob())
+        .then((blob)=>  {
+            const url = window.URL.createObjectURL(
+                new Blob([blob]),
+            );
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute(
+                'download',
+                doc.filename
+                
+            );
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+        })
+    }
 
     return (
         <div>
-            <div>
-                {instruments.map((i, index) => {
-                    return (
-                        <div key={index}>
-                            <div style={{display: "flex"}}>
-                            <h4>{i.name}</h4>
-                            <button onclick={()=> {}}></button>
+                <div>
+                    <label>Name </label>
+                        <div style={{color: "black"}}>
+                            <Select options={options}
+                            value={options.find(x => x.value === selectedValue)}
+                            onChange={handleInstrumentChange}
+                            />
+                        </div>
+                            <div>
+                                {show ? pdf.map((doc)=> (
+                                    <div key={doc.id}>
+                                        <h6>{doc.filename}</h6>
+                                        <button onClick={() => handleDownload(`instruments/${selectedValue}/pdfs/${doc.id}`, doc)}>Download</button>
                                     </div>
-                        {i.pdfs.map((p, i)=> (
-                            <article key={i}>
-                                <h6>{p.filename}</h6>
-                                <button >Download Pdf</button>
-                            </article>
-                        ))}
-                   </div>
-                    )
-                })}
+                                )): null}
+                            </div>
+
+
+
+
             </div>
         </div>
     )

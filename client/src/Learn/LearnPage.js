8f619bf20f3391acaa1f3a1a5b005448ae544711
thakaj <taulant.hakaj@scranton.edu>
2021-10-27 16:32:34 -0400

    I edited the page to only render the document mapping component

--- client/src/Learn/LearnPage.js
+++ client/src/Learn/LearnPage.js
@@ -1,13 +1,10 @@
 import React from "react";
-import LearnPageResources from "./LearnPageResources";
 import LearnMappedPdfs from "./LearnMappedPdfs";
 
 function LearnPage(){
 
     return (
         <div>
-            Hello Learn!!!!!!!!!!!
-            <LearnPageResources />
             <LearnMappedPdfs />
         </div>
     )

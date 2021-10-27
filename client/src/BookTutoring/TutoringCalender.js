43b37ffc95d800c7d5c56f25170bd01043463567
thakaj <taulant.hakaj@scranton.edu>
2021-10-27 16:30:11 -0400

    trying to get the tutoring calendar in order. I honestly need to sit down and think it through as to how I want to this part of the page to function

--- client/src/BookTutoring/TutoringCalender.js
+++ client/src/BookTutoring/TutoringCalender.js
@@ -1,14 +1,35 @@
 import React from "react";
-import FullCalendar from '@fullcalendar/react'
+import FullCalendar, {formatDate} from '@fullcalendar/react'
 import interactionPlugin from '@fullcalendar/interaction'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import "@fullcalendar/daygrid/main.css";
 
 function TutoringCalender(){
-    
+ 
+  let str = formatDate('2018-09-01', {
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'UTC',
+    locale: 'es'
+  })
+
     return(
         <div>
-          <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin, interactionPlugin ]}
-          initialView="dayGridMonth" />
+          <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin, interactionPlugin, timeGridPlugin ]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+          />
         </div>    
     )
 }
